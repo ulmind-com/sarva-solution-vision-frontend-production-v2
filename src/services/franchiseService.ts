9ccadce33fa10b35d1a9f7827d85c3b66d15ab14
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2026-03-11 01:21:56 +0530

    changes of api

--- src/services/franchiseService.ts
+++ src/services/franchiseService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://sarvasolution-backend.onrender.com';
 
 // Create a separate axios instance for franchise APIs that uses franchise token
 const franchiseApi = axios.create({
